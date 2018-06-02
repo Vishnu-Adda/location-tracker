6fab282fe063109a3dce330215b24836f2507098
Vishnu Adda <addavishnu@gmail.com>
2018-06-01 18:29:21 -0700

    Finished the locating and updating of the 5 descriptions.

--- app/src/main/java/com/someapp/vishnu/myapphikerswatch/MainActivity.java
+++ app/src/main/java/com/someapp/vishnu/myapphikerswatch/MainActivity.java
@@ -3,6 +3,8 @@ package com.someapp.vishnu.myapphikerswatch;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -13,11 +15,38 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     LocationManager locationManager;
     LocationListener locationListener;
+
+    TextView latitudeText;
+    TextView longitudeText;
+    TextView accuracyText;
+    TextView altitudeText;
+    TextView addressText;
+
+    final static String LATITUDE_STRING;
+    final static String LONGITUDE_STRING;
+    final static String ACCURACY_STRING;
+    final static String ALTITUDE_STRING;
+    final static String ADDRESS_STRING;
+
+    static {
+
+        LATITUDE_STRING = "Latitude: ";
+        LONGITUDE_STRING = "Longitude: ";
+        ACCURACY_STRING = "Accuracy: ";
+        ALTITUDE_STRING = "Altitude: ";
+        ADDRESS_STRING = "Address: ";
+
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
@@ -48,6 +77,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        latitudeText = findViewById(R.id.latitudeText);
+        longitudeText = findViewById(R.id.longitudeText);
+        accuracyText = findViewById(R.id.accuracyText);
+        altitudeText = findViewById(R.id.altitudeText);
+        addressText = findViewById(R.id.addressText);
+
         locationManager =
                 (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
@@ -55,7 +90,40 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onLocationChanged(Location location) { // Regular updates from GPS change
 
-                Log.i("Location", location.toString());
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+                try {
+                    List<Address> listAddresses =  geocoder.getFromLocation(location.getLatitude(),
+                            location.getLongitude(), 1);
+
+                    latitudeText.setText(LATITUDE_STRING +
+                            Float.toString((float)location.getLatitude()));
+
+                    longitudeText.setText(LONGITUDE_STRING +
+                            Float.toString((float)location.getLongitude()));
+
+                    accuracyText.setText(ACCURACY_STRING +
+                            Integer.toString((int)location.getAccuracy()) + " meters");
+
+                    altitudeText.setText(ALTITUDE_STRING +
+                            Integer.toString((int)location.getAltitude()) + " meters" );
+
+                    if(listAddresses != null && listAddresses.size() > 0) {
+
+                        if (listAddresses.get(0).getLocality() != null
+                                && listAddresses.get(0).getAdminArea() != null) {
+
+                            addressText.setText(ADDRESS_STRING +
+                                    listAddresses.get(0).getLocality() + " "
+                                    + listAddresses.get(0).getAdminArea());
+
+                        }
+
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
             }
 
@@ -83,7 +151,6 @@ public class MainActivity extends AppCompatActivity {
                     && ActivityCompat.checkSelfPermission(this,
                     Manifest.permission.ACCESS_COARSE_LOCATION)
                     != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
                 //    ActivityCompat#requestPermissions
                 // here to request the missing permissions, and then overriding
                 //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
@@ -109,6 +176,44 @@ public class MainActivity extends AppCompatActivity {
 
                 locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
                         0, 0, locationListener);
+
+                Location lastKnownLocation =
+                        locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+                try {
+                    List<Address> listAddresses =  geocoder.getFromLocation(lastKnownLocation.getLatitude(),
+                            lastKnownLocation.getLongitude(), 1);
+
+                    latitudeText.setText(LATITUDE_STRING +
+                            Float.toString((float)lastKnownLocation.getLatitude()));
+
+                    longitudeText.setText(LONGITUDE_STRING +
+                            Float.toString((float)lastKnownLocation.getLongitude()));
+
+                    accuracyText.setText(ACCURACY_STRING +
+                            Integer.toString((int)lastKnownLocation.getAccuracy()) + " meters");
+
+                    altitudeText.setText(ALTITUDE_STRING +
+                            Integer.toString((int)lastKnownLocation.getAltitude()) + " meters" );
+
+                    if(listAddresses != null && listAddresses.size() > 0) {
+
+                        if (listAddresses.get(0).getLocality() != null
+                                && listAddresses.get(0).getAdminArea() != null) {
+
+                            addressText.setText(ADDRESS_STRING +
+                                    listAddresses.get(0).getLocality() + " "
+                                    + listAddresses.get(0).getAdminArea());
+
+                        }
+
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
             }
         }
